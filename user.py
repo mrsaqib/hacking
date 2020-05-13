83514a8991dca39f4d5fd504b233bbee8ac85fb7
mrsaqib <65269214+mrsaqib@users.noreply.github.com>
2020-05-13 09:47:18 +0500

    Update user.py

--- user.py
+++ user.py
@@ -56,9 +56,9 @@ logo = """ -----------------------------•◈•
 |__~~~\ •◈•-----█-------⑧-------█------•◈•
 |__~~~\ •◈•-----█-------⑥-------█------•◈•
 \033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;FlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1; Aahilrna4072
+\033[1;96mAuthor  \033[1;93m: \033[1;92Saqib ur rehman
+\033[1;96mInstagram \033[1;93m: \033[1;Rohim khan
+\033[1;96mFacebook  \033[1;93m: \033[1; Saqib ur rehman
 \033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/Therana/zero
 \033[1;91m======================================="""
 
@@ -80,10 +80,10 @@ vuln = "\033[32mVuln"
 os.system("clear")
 print "\033[1;96m ============================================================="
 print  """\033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;92mFlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1;92m Aahilrana4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/Therana/zero
+\033[1;96mAuthor  \033[1;93m: \033[1;92Sqib ur rehman
+\033[1;96mInstagram \033[1;93m: \033[1;92Pakhtoon
+\033[1;96mFacebook  \033[1;93m: \033[1;92m Rohim khan
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/saqib/Khan
 \033[1;91m======================================="""
 print " \x1b[1;93m============================================================="
 
@@ -100,10 +100,10 @@ while (loop == 'true'):
             loop = 'false'
         else:
             print "Wrong Password"
-            os.system('xdg-open https://www.Youtube.com/UCsdJQbRf0xpvwaDu1rqgJuA')
+            os.system('xdg-open https://https://www.facebook.com/100011376485228
     else:
         print "Wrong Username"
-        os.system('xdg-open https://www.Youtube.com/UCsdJQbRf0xpvwaDu1rqgJuA')
+        os.system('xdg-open https://https://www.facebook.com/profile.php?id=100012155403009
 
 def login():
 	os.system('clear')
